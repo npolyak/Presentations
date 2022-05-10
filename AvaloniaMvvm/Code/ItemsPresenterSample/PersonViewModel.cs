72e9ca7a829f1c8d831818b1ddd831edbc6c8b30
npolyak <nick.polyak@verizon.net>
2022-05-09 22:56:02 -0400

    added sample

---
+++ AvaloniaMvvm/Code/ItemsPresenterSample/PersonViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+
+namespace ItemsPresenterSample
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // fires notification if a property changes
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public PersonViewModel(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        #region IsSelected Property
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get
+            {
+                return this._isSelected;
+            }
+            set
+            {
+                if (this._isSelected == value)
+                {
+                    return;
+                }
+
+                this._isSelected = value;
+                this.OnPropertyChanged(nameof(IsSelected));
+            }
+        }
+        #endregion IsSelected Property
+
+        public void ToggleSelection()
+        {
+            this.IsSelected = !this.IsSelected;
+        }
+    }
+}
